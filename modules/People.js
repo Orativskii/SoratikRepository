a4812f6ef196f1edf716af70eb6d16902738665a
Sorat <soratik35@gmail.com>
2021-01-06 19:29:08 +0200

    1

--- modules/People.js
+++ modules/People.js
@@ -1,53 +1,110 @@
 class People {
 
+    //Не дивись на мене так, я сам не понімаю
     #peopleCount;
     #balance;
     #menCount;
     #womenCount;
+    #pregnancy;
+    #pregnancyWomen;
+    #bornPeople;
+    thisWeek = 0;
+    dateBorn = [];
 
     constructor(resources) {
         this.#updateInput(resources);
         this.#menCount = this.getRandomValue(this.#peopleCount * 0.4, this.#peopleCount * 0.6);
         this.#womenCount = this.#peopleCount - this.#menCount;
+        this.#pregnancy = 3;
+        this.#pregnancyWomen = 0;
+        this.#bornPeople = 0;
     }
 
 
     oneYear(res, time) {
-        this.#peopleCount += Math.trunc(this.#peopleCount * res.getRNI());
-        this.getGender(res);
+        // this.#peopleCount += Math.trunc(this.#peopleCount * res.getRNI());
+
+        this.pregnancyWomen();
+        this.getDateBorn();
         this.showData(res);
         this.#updateOutput(res);
-        console.log()
+
+
     }
 
     oneMonth(res, time) {
-        res.setRNI(res.getRNI() + 0.01);
-        res.setLifeQuality(res.getLifeQuality() + 0.03);
-        this.#balance += this.#peopleCount * 5;
+        // res.setRNI(res.getRNI() + 0.01);
+        // res.setLifeQuality(res.getLifeQuality() + 0.03);
+        this.giveTax();
 
     }
+
+
 
     oneWeek(res, time) {
+        this.thisWeek++;
+        if (this.thisWeek > 52) this.thisWeek = 0;
+        this.born();
+    }
 
+    pregnancyWomen() {
+        let i = 0;
+        while (i < this.#womenCount) {
+            let chancePregnancy = this.getRandomValue(0, 100);
+            if (chancePregnancy <= this.#pregnancy) {
+                this.#pregnancyWomen++;
+            }
+            i++;
+        }
+    }
+
+    born() {
+        let i;
+
+            this.dateBorn.forEach(i => {
+                if (this.thisWeek === i) {
+                    this.#peopleCount++;
+                    this.#bornPeople++;
+                    this.getGender();
+            }
+
+        });
     }
 
 
-    getGender(res) {
-        for (let i = 0; i < this.#peopleCount - res.getPeopleCount(); i++) {
-            let gender = Math.floor(this.getRandomValue(0, 2));
-            gender === 1 ? this.#menCount++ : this.#womenCount++;
+    getDateBorn() {
+        this.dateBorn.length = 0;
+        if (this.#pregnancyWomen > 0) {
+            for (let i = 0; i < this.#pregnancyWomen; i++) {
+                let dateBornPeople = this.getRandomValue(1, 53);
+                this.dateBorn[i] = dateBornPeople;
+            }
+            this.#pregnancyWomen = 0;
         }
+        this.dateBorn.sort( (a, b) => a - b);
+    }
+
+    getGender(res) {
+        let gender = Math.floor(this.getRandomValue(0, 2));
+        gender === 1 ? this.#menCount++ : this.#womenCount++;
+    }
+        thisYearTax = 0;
+// плоти нолог
+    giveTax() {
+        this.thisYearTax = this.#peopleCount * 5;
+        this.#balance += this.thisYearTax;
     }
 
     showData(res) {
-        if (this.#peopleCount > res.getPeopleCount()) {
-            console.log("New peoples: " + (this.#peopleCount - res.getPeopleCount()));
-        }
+        console.log(this.dateBorn.length + " pregnancy women");
+        console.log(this.#bornPeople + " born people for the year");
+        this.#bornPeople = 0;
         console.log(this.#menCount + " mens");
         console.log(this.#womenCount + " womens");
-        console.log("New tax money for the year: " + (this.#balance - res.getBalance()) + "$");
+        console.log("New tax money for the year: " + this.thisYearTax + "$");
         console.log("Balance city: " + this.#balance + "$");
     }
+
 
     getRandomValue(min, max) {
         min = Math.ceil(min);
